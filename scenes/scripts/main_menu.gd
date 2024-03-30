b1554e5d34bdf21ca491549dcff38ab62adefabf
LeoRussell <leo.russell@mail.ru>
2024-03-30 18:48:43 +0300

    added main game node

--- scenes/scripts/main_menu.gd
+++ scenes/scripts/main_menu.gd
@@ -1,10 +1,11 @@
 extends Control
 
+@export_file("*.tscn") var _game
 
 func _input(event): 
 	if event.as_text().to_upper() == "SPACE":
-		queue_free()
+		get_tree().change_scene_to_file(_game) 
 		
 	elif event.as_text().to_upper() == "ESCAPE":
 		get_tree().quit()
-	
+ 
